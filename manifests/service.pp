852e023a519672272fc2ff6903a86e34871de444
Jordi Prats <jprats@systemadmin.es>
2016-03-02 11:55:40 +0100

    condicions fact docker

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,8 +9,9 @@ class iptables::service (
   validate_bool($manage_service)
   validate_bool($enable)
 
-  if($::eyp_docker_iscontainer==undef or
-    $::eyp_docker_iscontainer==false or
+  if($eyp_docker_iscontainer==undef or
+    $eyp_docker_iscontainer==false or
+    $eyp_docker_iscontainer=~ /false/ or
     $manage_docker_service)
   {
     if($manage_service)
