06a0d30425afe2622c414fb570157a9f6c6e7eb8
Jordi Prats <jprats@systemadmin.es>
2016-10-18 13:23:01 +0200

    oc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
 # CHANGELOG
 
+## 0.1.10
+
+* * logrotate configuration file using eyp-logrotate (manage_logrotate=>false to disable)
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@ class iptables  (
                   $enable                = $iptables::params::service_enable_default,
                   $manage_docker_service = false,
                   $manage_service        = true,
+                  $manage_logrotate      = true,
+                  $logrotate_rotate      = '4',
+                  $logrotate_compress    = true,
+                  $logrotate_missingok   = true,
+                  $logrotate_notifempty  = true,
+                  $logrotate_frequency   = 'weekly',
                 ) inherits iptables::params {
 
   if(!defined(Class['::firewalld']))
@@ -31,6 +37,19 @@ class iptables  (
       manage_docker_service => $manage_docker_service,
       manage_service        => $manage_service,
       require               => Package[$iptables::params::iptables_pkgs],
+    }
+
+    if($manage_logrotate)
+    {
+      logrotate::logs { 'iptables':
+        ensure        => present,
+        log           => [ '/var/log/iptables.log' ],
+        rotate        => $logrotate_rotate,
+        compress      => $logrotate_compress,
+        missingok     => $logrotate_missingok,
+        notifempty    => $logrotate_notifempty,
+        frequecy      => $logrotate_frequency,
+      }
     }
   }
 }
