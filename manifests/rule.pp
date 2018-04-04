a5f94ed73bba005f72c190542822931563c570ff
Jordi Prats <jordi.prats@gmail.com>
2018-04-04 13:27:00 +0200

    added description

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -2,14 +2,15 @@
 # - Allow HTTPS
 # - Allow communication between the 3 VM with TCP and UDP protocols on the port range 9300 to 9400.
 # - Allow communication with 192.168.188.142 with TCP protocol on port 9200), this 3 VM must communicate with the VIP elasticsearch-eupp (IP: 192.168.188.142 ).
-define rule (
-              $chain      = 'INPUT',
-              $target     = 'REJECT',
-              $protocol   = undef,
-              $dport      = undef,
-              $order      = '42',
-              $persistent = true,
-            ) {
+define iptables::rule (
+                        $description = $name,
+                        $chain       = 'INPUT',
+                        $target      = 'REJECT',
+                        $protocol    = undef,
+                        $dport       = undef,
+                        $order       = '42',
+                        $persistent  = true,
+                      ) {
   include ::iptables
 
   if($iptables::params::iptablesrulesetfile==undef)
